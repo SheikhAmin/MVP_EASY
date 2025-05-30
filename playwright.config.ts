4740a455424076fe8b767742a189b4570a476d97
Assaduzzaman <assad@ael.net>
2025-05-30 14:52:13 +0600

    some fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless:false,
+    headless:true,
     baseURL: "http://localhost:8087/Account/Login?ReturnUrl=%2f",
     browserName: 'firefox'
   },
