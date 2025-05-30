4e5b3ac67bcdbe646bb4b46124bcf2c403fd40ae
Assaduzzaman <assad@ael.net>
2025-05-30 14:59:12 +0600

    added a script

---
+++ tests/dryrun.spec.ts
@@ -0,0 +1,14 @@
+import { beforeEach, describe } from "node:test";
+import { DealerRegistration } from "../Pom/DealerRegistration";
+import { queryLatestUser } from "../database/fetchCredentials";
+import { Common } from "../Pom/Common";
+
+import { test, expect } from "@playwright/test";
+
+describe("Test Cases",()=>{
+
+    test("Dry run",async ({page})=>{
+        page.goto("https://www.youtube.com/");
+    })
+
+})
